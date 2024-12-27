1ecb27ed4563211b9b2dc5b9d093d1f8a7f45c48
Kyle Franz <kyle@basisrobotics.tech>
2024-12-27 13:53:20 -0800

    undo changes to header includes

--- tf2/include/tf2/buffer_core.hpp
+++ tf2/include/tf2/buffer_core.hpp
@@ -44,13 +44,13 @@
 #include <utility>
 #include <vector>
 
-#include "LinearMath/Transform.h"
+#include "LinearMath/Transform.hpp"
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "geometry_msgs/msg/velocity_stamped.hpp"
 #include "rcutils/logging_macros.h"
-#include "tf2/buffer_core_interface.h"
-#include "tf2/exceptions.h"
-#include "tf2/transform_storage.h"
+#include "tf2/buffer_core_interface.hpp"
+#include "tf2/exceptions.hpp"
+#include "tf2/transform_storage.hpp"
 #include "tf2/visibility_control.h"
 
 namespace tf2
